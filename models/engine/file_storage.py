baf67ceb88a0be39e9d7f949febb21ff84916466
kiradukund <k.iradukund@alustudent.com>
2026-09-19 02:41:35 -0700

    Fix PEP8 line too long in file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,7 +4,7 @@ import json
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON to instances."""
+    """Serializes instances to JSON file and deserializes JSON to instances."""
 
     __file_path = "file.json"
     __objects = {}
